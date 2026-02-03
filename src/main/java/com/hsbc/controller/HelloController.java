1c40640f3087854b0e5e36e9ed94fec5fc86b99a
KishorgnAI <kishorgn.ai@gmai.com>
2026-02-03 05:16:43 +0000

    Simple change has done

--- src/main/java/com/hsbc/controller/HelloController.java
+++ src/main/java/com/hsbc/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 public class HelloController {
     @GetMapping("/")
     public String sayHelloGet(){
-        return "Hello - GET";
+        return "Hello - by get()";
     }
 
     @PostMapping
